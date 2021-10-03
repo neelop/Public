25b306361200f1e0bab385fa1e857b9bb39c21ca
neelop <pwrzaszcz@gmail.com>
2021-10-03 21:25:00 +0200

    commit operation

--- projecteuler.net/042_CodedTriangleNumbers.py
+++ projecteuler.net/042_CodedTriangleNumbers.py
@@ -9,28 +9,49 @@ def main():
     b[0] = b[0][1:]
     b[-1] = b[-1][:-1]
 
-    max = 0
+    maks = 0
     for word in b:
-        sum = 0
+        suma = 0
         for letter in word:
-            sum += ord(letter) - 64
-        if sum > max:
-            max = sum
+            suma += ord(letter) - 64
+        if suma > maks:
+            maks = suma
     triangle_numbers = []
     x, n = 1, 1
-    while x <= max:
+    while x <= maks:
         x = triangle_n(n)
         triangle_numbers.append(x)
         n += 1
     counter = 0
     for word in b:
-        sum = 0
+        suma = 0
         for letter in word:
-            sum += ord(letter) - 64
-        if sum* 1.0 in triangle_numbers:
-            counter +=1
+            suma += ord(letter) - 64
+        if suma * 1.0 in triangle_numbers:
+            counter += 1
     print(counter)
 
+def main2():
+    f = open("p042_words.txt", "r")
+    for x in f:
+        b = x.split('","')
+    b[0] = b[0][1:]
+    b[-1] = b[-1][:-1]
+
+    counter = 0
+    triangle_numbers = [1]
+    x, n = 1, 2
+    for word in b:
+        suma = 0
+        for letter in word:
+            suma += ord(letter) - 64
+        while suma > triangle_numbers[-1]:
+            x = triangle_n(n)
+            triangle_numbers.append(x)
+            n += 1
+        if suma * 1.0 in triangle_numbers:
+            counter += 1
+    print(counter)
 
 if __name__ == '__main__':
-    main()
+    main2()
