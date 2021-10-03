4055e1edc5600f48f06d3453c28bf53b1f1e97fe
neelop <pwrzaszcz@gmail.com>
2021-10-03 22:19:55 +0200

    commit operation

--- projecteuler.net/043_SubStringDiv.py
+++ projecteuler.net/043_SubStringDiv.py
@@ -9,22 +9,22 @@ def main():
         i = "".join(i)
         if i[0] != "0":
             marker = 0
-            while marker == 0:
-                if int(i[1] + i[2] + i[3]) % 2 != 0:
-                    marker += 1
-                elif int(i[2] + i[3] + i[4]) % 3 != 0:
-                    marker += 1
-                elif int(i[3] + i[4] + i[5]) % 5 != 0:
-                    marker += 1
-                elif int(i[4] + i[5] + i[6]) % 7 != 0:
-                    marker += 1
-                elif int(i[5] + i[6] + i[7]) % 11 != 0:
-                    marker += 1
-                elif int(i[6] + i[7] + i[8]) % 13 != 0:
-                    marker += 1
-                elif int(i[7]+i[8]+i[9]) % 17 != 0:
-                    marker += 1
-                break
+            
+            if int(i[1] + i[2] + i[3]) % 2 != 0:
+                marker += 1
+            elif int(i[2] + i[3] + i[4]) % 3 != 0:
+                marker += 1
+            elif int(i[3] + i[4] + i[5]) % 5 != 0:
+                marker += 1
+            elif int(i[4] + i[5] + i[6]) % 7 != 0:
+                marker += 1
+            elif int(i[5] + i[6] + i[7]) % 11 != 0:
+                marker += 1
+            elif int(i[6] + i[7] + i[8]) % 13 != 0:
+                marker += 1
+            elif int(i[7]+i[8]+i[9]) % 17 != 0:
+                marker += 1
+
             if marker == 0:
                 suma += int(i)
                 print(i)
